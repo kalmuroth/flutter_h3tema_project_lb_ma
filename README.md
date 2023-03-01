985e7319996ace192569d25819114cef96ba6715
kalmuroth <lucas.bordner@cloudartifact.com>
2023-03-01 12:29:57 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_h3tema_project_lb_ma
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_h3tema_project_lb_ma/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_h3tema_project_lb_ma
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
