3a12056cc90fb7c5d780f02c87a8c308684fca9a
kalmuroth <lucas.bordner@cloudartifact.com>
2023-03-03 14:32:12 +0100

    View screen 1 update

--- lib/countryList.dart
+++ lib/countryList.dart
@@ -27,73 +27,97 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(title: Text('Select Country')),
         body: FutureBuilder(
-      future: ReadJsonData(),
-      builder: (context, data) {
-        if (data.hasError) {
-          return Center(child: Text("${data.error}"));
-        } else if (data.hasData) {
-          var items = data.data as List<Country>;
-          return ListView.builder(
-              itemCount: items == null ? 0 : items.length,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () => (_navigateToNextScreen(
-                      context, items[index].capital.toString())),
-                  child: Card(
-                    elevation: 5,
-                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-                    child: Container(
-                      padding: EdgeInsets.all(8),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            child: Image(
-                              image: NetworkImage(
-                                  items[index].flags.png.toString()),
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                          Expanded(
-                              child: Container(
-                            padding: EdgeInsets.only(bottom: 8),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.only(left: 8, right: 8),
-                                  child: Text(
-                                    items[index].capital.toString(),
-                                    style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold),
-                                  ),
+          future: ReadJsonData(),
+          builder: (context, data) {
+            if (data.hasError) {
+              return Center(child: Text("${data.error}"));
+            } else if (data.hasData) {
+              var items = data.data as List<Country>;
+              return ListView.builder(
+                  itemCount: items == null ? 0 : items.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () => (_navigateToNextScreen(
+                          context, items[index].capital.toString())),
+                      child: Card(
+                        elevation: 5,
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                width: 106,
+                                height: 71,
+                                child: Image(
+                                  image: NetworkImage(
+                                      items[index].flags.png.toString()),
+                                  fit: BoxFit.fill,
                                 ),
-                                Padding(
-                                  padding: EdgeInsets.only(left: 8, right: 8),
-                                  child: Text(items[index].region.toString()),
-                                )
-                              ],
-                            ),
-                          ))
-                        ],
+                              ),
+                              Expanded(
+                                  child: Container(
+                                padding: EdgeInsets.only(bottom: 8),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding:
+                                          EdgeInsets.only(left: 8, right: 8),
+                                      child: Text(
+                                        (items[index]
+                                            .capital
+                                            .toString()
+                                            .replaceAll(
+                                                new RegExp(r'[^\w\s]+'), '')),
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding:
+                                          EdgeInsets.only(left: 8, right: 8),
+                                      child: Text(
+                                          (items[index]
+                                                  .name
+                                                  .common
+                                                  .toString()
+                                                  .replaceAll(
+                                                      new RegExp(r'[^\w\s]+'),
+                                                      '') +
+                                              " - " +
+                                              items[index]
+                                                  .subregion
+                                                  .toString()
+                                                  .replaceAll(
+                                                      new RegExp(r'[^\w\s]+'),
+                                                      '')),
+                                          style: TextStyle(
+                                              color: Colors.grey[400])),
+                                    )
+                                  ],
+                                ),
+                              ))
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                );
-              });
-        } else {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-      },
-    ));
+                    );
+                  });
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ));
   }
 
   void _navigateToNextScreen(BuildContext context, countryItem) {
